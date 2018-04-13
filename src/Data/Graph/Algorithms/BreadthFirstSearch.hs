08a8ed1dd4d7dc94d37510c8a08c85c97c4f21ae
Bhavin Shah <bns8487@rit.edu>
2018-04-13 14:57:10 -0400

    added all code in single file

--- src/Data/Graph/Algorithms/BreadthFirstSearch.hs
+++ src/Data/Graph/Algorithms/BreadthFirstSearch.hs
@@ -41,71 +41,10 @@ import Data.Set as S
 --finishVertex :: g a -> a ->  g a
 
 
-bfsFold :: (Graph g a) => g a -> b -> [a]           -- source nodes
-                                   -> (a -> b -> b) -- visit node
-                                   -> (a -> b -> b) -- revisit node
-                                   -> b
-bfsFold graph acc queue fv fr = bfsFold' graph acc queue fv fr S.empty
-
-bfsFold' :: (Graph g a) => g a -> b -> [a]           -- source nodes
-                                    -> (a -> b -> b) -- visit node
-                                    -> (a -> b -> b) -- revisit node
-                                    -> S.Set a   -- visited nodes
-                                    -> b
-bfsFold' graph acc queue fv fr vSet =
-  case queue of
-    []     -> acc
-    (x:xs) -> case (S.member x vSet) of
-                True  -> bfsFold' graph (fr x acc) xs fv fr vSet
-                False -> bfsFold' graph (fv x acc) (xs ++ (neighbors graph x)) fv fr (S.insert x vSet)
-
---
-bfs :: (Graph g a) => g a -> [a] -> [a]
-bfs g sx = bfsFold g [] sx (\x acc -> acc ++ [x]) (\_ acc -> acc)
 
 
--- A list of nodes reachable from a given node.
-reachable :: (Graph g a) => g a -> a -> [a]
-reachable g x = bfsFold g [] [x] (\n acc -> n:acc) (\_ -> id)
-
---Is the second vertex reachable from the first?
-path :: (Graph g a) => g a -> a -> a -> Bool
-path g x y = bfsFold g False [x] (\n acc -> acc || n == y) (\_ -> id)
-
-isCyclic :: (Graph g a) => g a -> [a] -> Bool
-isCyclic g sx = bfsFold g False sx (\_ -> id) (\_ _ -> True)
-
---A topological sort of the graph. The order is partially specified by the condition
--- that a vertex i precedes j whenever j is reachable from i but not vice versa.
-topSort :: (Graph g a) => g a -> [a]
-topSort = undefined
-
---The connected components of a graph. Two vertices are connected if there is a path
--- between them, traversing edges in either direction.
-components :: (Graph g a) => g a -> [g a] --since we do not have tree rep, else [Tree t a]
-components = undefined
-
---The strongly connected components of a graph.
-scc :: (Graph g a) => g a -> [g a]
-scc = undefined
-
---The biconnected components of a graph. An undirected graph is
--- biconnected if the deletion of any vertex leaves it connected.
-bcc :: (Graph g a) => g a -> [g a]
-bcc = undefined
-
---The graph obtained by reversing all edges.
-transposeG :: (Graph g a) => g a -> g a
-transposeG = undefined
 
 
---How to do this?
-----------------------------------------------------------------
---A table of the count of edges from each node.
---outdegree :: Graph -> Table Int
-
---indegree :: Graph -> Table Int
---A table of the count of edges into each node.
 
 
 
