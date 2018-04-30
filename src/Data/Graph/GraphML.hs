4cc5d541a9d461826c81102b323f07f8c8b58903
Anindo Ashim Saha <anindoasaha@gmail.com>
2018-04-30 13:21:51 -0400

    Comments.

--- src/Data/Graph/GraphML.hs
+++ src/Data/Graph/GraphML.hs
@@ -17,14 +17,23 @@ import Text.XML.HXT.Core
 import qualified Data.Graph as DataGraph
 import qualified Data.Graph.Class as GraphLib
 
+{-
+    Data structure to hold parsed XML data.
+-}
 data Graph = Graph
   { graphId :: String,
     nodes :: [String],
     edges :: [(String, String, Double)]
   }
 
+-----------------------------------------
+-- Get element by tag name
+-----------------------------------------
 tag name = deep (isElem >>> hasName name)
 
+{-
+    Extract <edge> tags
+-}
 extractEdge [] = tag "edge" >>>
   proc e -> do
       source <- getAttrValue "source" -< e
@@ -35,11 +44,15 @@ extractEdge [id] = tag "edge" >>>
   proc e -> do
       source <- getAttrValue "source" -< e
       target <- getAttrValue "target" -< e
-      weight <- extractWeight id -< e
+      -- <data key="d1">1.1</data>
+      weight <- (extractWeight id) {-`orElse` constA 1.0-} -< e
       returnA -< (source, target, weight)
 
 -- fname <- getText <<< getChildren <<< deep (hasName "fname") -< x
 
+{-
+    Extract weight from <data> tag according to given key.
+-}
 extractWeight id = tag "data" >>> hasAttrValue "key" (\v -> v == id) >>>
     proc k -> do
         w <- getText <<< getChildren -< k
@@ -51,20 +64,27 @@ getWeightKey attr =
   (getAttrValue attr >>> isA weight >>> arr Just)
     `orElse` (constA Nothing)
 
--- <key id="d1" for="edge" attr.name="weight" attr.type="double"/>
--- <data key="d1">1.1</data>
-
+{-
+    Extract <key> data as per for and key filters
+    <key id="d1" for="edge" attr.name="weight" attr.type="double"/>
+-}
 extractKey for key = tag "graphml" >>> tag "key" >>> hasAttrValue "attr.name" (\v -> v == key)
                        >>> hasAttrValue "for" (\v -> v == for) >>>
     proc k -> do
         id <- getAttrValue "id" -< k
         returnA -< id
 
+{-
+    Extract <node> tags
+-}
 extractNode = tag "node" >>>
   proc n -> do
       nodeId <- getAttrValue "id" -< n
       returnA -< nodeId
 
+{-
+    Extract <graph> tags
+-}
 extractGraph keys = tag "graph" >>>
   proc g -> do
       graphId <- getAttrValue "id" -< g
@@ -72,29 +92,43 @@ extractGraph keys = tag "graph" >>>
       edges <- listA (extractEdge keys) -< g
       returnA -< Graph{graphId=graphId, nodes=nodes, edges=edges}
 
+{-
+    Read GraphML.
+-}
 readGraphMLString :: String -> IO [GraphLib.DGraph String]
 readGraphMLString xml = do
     keys <- runX (readString [withValidate no] "graphml2.xml" >>> extractKey "edge" "weight")
     graphs <- runX (readString [withValidate no] xml >>> extractGraph [])
     return (map mkGraph graphs)
 
+{-
+    Read GraphML from file.
+-}
 readGraphMLFile :: String -> IO [GraphLib.DGraph String]
 readGraphMLFile file = do
     keys <- runX (readDocument [withValidate no] "graphml2.xml" >>> extractKey "edge" "weight")
     graphs <- runX (readDocument [withValidate no] file >>> extractGraph keys)
     return (map mkGraph graphs)
 
+{-
+    Write GraphML to file.
+-}
 writeGraphMLFile :: GraphLib.DGraph String -> String -> IO [XmlTree]
 writeGraphMLFile g file = do
       runX (root [] [(graphToXmlTree g)] >>>
                      writeDocument [withIndent yes] file)
-
+{-
+    Create GraphLib.DGraph from Graph
+-}
 mkGraph :: Graph -> GraphLib.DGraph String
 mkGraph g = g'
     where
     g' = foldl (GraphLib.createNode) g'' (nodes g)
     g'' = (GraphLib.createFromEdges (map (\(s,t,w) -> GraphLib.Edge (s,t) w) (edges g)))
 
+{-
+    Create HXT XmlTree from GraphLib.DGraph
+-}
 graphToXmlTree g = mkelem "graphml" mkSchemaAttr
                                     [ mkelem "key" [ sattr "id" "d0", sattr "for" "edge", sattr "attr.name" "weight", sattr "attr.type" "double" ] [],
                                       mkelem "graph" [ sattr "edgedefault" "directed"]
@@ -112,7 +146,9 @@ graphToXmlTree g = mkelem "graphml" mkSchemaAttr
                                                         "http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd" ++
                                                         "http://graphml.graphdrawing.org/xmlns ") ]
 
-
+{-
+    main function for testing
+-}
 main :: IO ()
 main = do
     keys <- runX (readDocument [withValidate no] "graphml2.xml" >>> extractKey "edge" "weight")
